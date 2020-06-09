7770438c50bf66f7ceea2d2276c7e2e69ffbf575
Juri Wiens <juriwiens@gmail.com>
2020-06-09 17:05:41 +0200

    Update deps and remove jest

--- src/batch_consumer.spec.ts
+++
@@ -1,7 +0,0 @@
-import { KafkaBatchConsumer } from "./batch_consumer"
-
-describe("KafkaBatchConsumer", () => {
-  it("", () => {
-    // const consumer = new KafkaBatchConsumer({ kafkaConfig: {} })
-  })
-})
